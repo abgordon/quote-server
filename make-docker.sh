ce645ed5806bd9c0b58a656ad6ddf23c03e28ce2
Andrew Gordon <andrew.b.gordon@colorado.edu>
2018-12-17 11:52:34 -0700

    docker image name

--- make-docker.sh
+++ make-docker.sh
@@ -1,3 +1,3 @@
 GOOS=linux GOARCH=amd64 go build .
-docker build . -t registry.vipertv.net/viper/inspo:0.1
-docker push registry.vipertv.net/viper/inspo:0.1
+docker build . -t registry.vipertv.net/viper/quote-server:0.1
+docker push registry.vipertv.net/viper/quote-server:0.1
