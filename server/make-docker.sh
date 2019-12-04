6bba95a8742b4a45afdd2397b1dbaab8c6232e3d
Andrew Gordon <agordo240@COSML-1686904.local>
2019-12-04 10:02:51 -0700

    readme, make-docker

--- server/make-docker.sh
+++ server/make-docker.sh
@@ -1,3 +1,3 @@
 GOOS=linux GOARCH=amd64 go build .
-docker build . -t abgordon/quote-server:0.1
-docker push abgordon/quote-server:0.1
+docker build . -t abgordon/quote-server:0.3
+docker push abgordon/quote-server:0.3
