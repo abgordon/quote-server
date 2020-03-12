2f8167f3905f4782c2a6457598711285bd7e0436
Andrew Gordon <abgordon@colorado.edu>
2020-03-12 14:23:37 -0600

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,7 +2,7 @@
 RUNNING=$(docker ps | grep abgordon/quote-server)
 if [ -z "$RUNNING" ];
 then
-  docker run -p 8080:8080 -d abgordon/quote-server:0.1
+  docker run -p 8080:8080 -d abgordon/quote-server:0.3
 fi
 
 curl localhost:8080
